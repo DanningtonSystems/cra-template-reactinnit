03d128a8171246eb824c12e689d9f507103eb36b
Daniel Hyders <hello@bean.codes>
2021-07-13 16:29:06 -0700

    Base

---
+++ src/assets/Core.js
@@ -0,0 +1,24 @@
+import './css/Core.css';
+
+function Core() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.pinimg.com/originals/15/7f/7c/157f7cf0829e0d81af460e3c6b47c4a4.gif" className="App-logo" alt="logo" />
+        <p>
+          If you see this, it means that the Core react component is working!
+        </p>
+        <a
+          className="App-link"
+          href="https://bean.codes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Back to bean.codes
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Core;
